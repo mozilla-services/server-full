732bf3cbba0e5aceeb49e7ba73e40e8db7615730
Tarek Ziade <tarek@ziade.org>
2010-10-02 17:52:59 +0200

    now fuzzing with an authenticated user as well

--- tests/fuzzer/fuzzer.py
+++ tests/fuzzer/fuzzer.py
@@ -40,24 +40,31 @@ import socket
 import random
 import string
 import sys
+import base64
 
 ALLOWED_CODES = ['404', '401', '200', '400']
 HOST = 'localhost'
 PORT = 5000
-PATHS = ['/user/1.0/__random__', '/user/1.0/__random__/node/weave',
+USER = 'test_user_96869'
+PASS = 'x' * 9
+PATHS = ['/user/1.0/__randomu__', '/user/1.0/__random__/node/weave',
          '/user/1.0/__random__/password_reset',
-         '/1.0/__random__/info/collections',
-         '/1.0/__random__/info/collection_usage',
-         '/1.0/__random__/info/collection_counts',
-         '/1.0/__random__/info/quota',
-         '/1.0/__random__/storage/__random__',
-         '/1.0/__random__/storage/__random__/__random__']
+         '/1.0/__randomu__/info/collections',
+         '/1.0/__randomu__/info/collection_usage',
+         '/1.0/__randomu__/info/collection_counts',
+         '/1.0/__randomu__/info/quota',
+         '/1.0/__randomu__/storage/__random__',
+         '/1.0/__randomu__/storage/__random__/__random__']
 
-def randheaders():
+def randheaders(auth=False):
     headers = ['User-Agent: Fuzzer']
-    if random.randint(1, 3) == 2:
+    if random.randint(1, 3) == 2 and not auth:
         # return bad auth header
         headers.append('Authorization: Basic %s' % randtext(10))
+    elif auth:
+        # Create a real valid auth
+        token = base64.encodestring('%s:%s' % (USER, PASS))
+        headers.append('Authorization: Basic %s' % token)
     if random.randint(1, 3) == 1:
         # return bad auth header
         headers.append('X-If-Unmodified-Since: %s' % randtext(10))
@@ -71,32 +78,42 @@ def randtext(size=None):
             size = random.randint(4, 15)
     return ''.join([random.choice(string.ascii_letters) for i in range(size)])
 
-def _GET():
+def _GET(auth):
     path = random.choice(PATHS)
     path = path.replace('__random__', randtext())
+    if auth:
+        path = path.replace('__randomu__', USER)
+    else:
+        path = path.replace('__randomu__', randtext())
+
     return "GET %s HTTP/1.0\r\nHost: %s\r\n%s\r\n\r\n" % \
-            (path, randheaders(), HOST)
+            (path, randheaders(auth), HOST)
 
-def _POST():
-    return _DATA()
+def _POST(auth):
+    return _DATA(auth)
 
-def _PUT():
-    return _DATA('PUT')
+def _PUT(auth):
+    return _DATA(auth, 'PUT')
 
-def _DELETE():
-    return _DATA('DELETE')
+def _DELETE(auth):
+    return _DATA(auth, 'DELETE')
 
-def _DATA(method='POST'):
+def _DATA(auth=False, method='POST'):
     path = random.choice(PATHS)
     path = path.replace('__random__', randtext())
+    if auth:
+        path = path.replace('__randomu__', USER)
+    else:
+        path = path.replace('__randomu__', randtext())
+
     data = randtext(random.randint(1, 1000))
     return "POST %s HTTP/1.0\r\nHost: %s\r\n%s\r\n\r\n%s" % \
-            (path, HOST, randheaders(), data)
+            (path, HOST, randheaders(auth), data)
 
-def build_request():
-    return random.choice([_GET, _POST, _PUT, _DELETE])()
+def build_request(auth):
+    return random.choice([_GET, _POST, _PUT, _DELETE])(auth)
 
-def call():
+def call(auth=False):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error, msg:
@@ -110,7 +127,7 @@ def call():
         print str(msg)
         sys.exit(2)
 
-    sock.send(build_request())
+    sock.send(build_request(auth))
     try:
         data = sock.recv(1024)
         res = ''
@@ -130,13 +147,10 @@ def call():
     except socket.error, e:
         print str(e)
 
-# anynymous calls
+# anonymous + auth calls
 for i in range(5000):
-    call()
+    call(random.randint(1, 3) == 2)
     sys.stdout.flush()
-
-# XXX need to fuzz with an authenticated user now
-#
 
 print 'Success'
 sys.exit(0)
