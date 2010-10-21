99f315c282193fc207d6b4809f3af714c790d3e2
Tarek Ziade <tarek@ziade.org>
2010-10-21 17:59:05 +0200

    added random delays

--- tests/delay/delay.py
+++ tests/delay/delay.py
@@ -2,27 +2,36 @@
 """
 import sys
 import time
+import random
 
 from twisted.internet import reactor
 from twisted.protocols import portforward
 
 CURRENT = 0.
-MAX_DELAY = 2.
-GROWTH = .2
+MAX_DELAY = 5.
+GROWTH = .1
+RANDOM_DELAYS = (2., 5., 7.)
+RANDOM_RATIO = 100.
+
 
 class LoggingProxyServer(portforward.ProxyServer):
 
     def dataReceived(self, data):
-        global CURRENT
-        global GROWTH
+        if random.randint(1, RANDOM_RATIO) == 1:
+            delay = random.choice(RANDOM_DELAYS)
+        else:
+            global CURRENT
+            global GROWTH
 
-        if CURRENT > MAX_DELAY:
-            GROWTH = -.2
-        elif CURRENT <= abs(GROWTH):
-            GROWTH = .2
-        CURRENT += GROWTH
-        print 'Delaying call for %.1fs' % CURRENT
-        time.sleep(CURRENT)
+            if CURRENT > MAX_DELAY:
+                GROWTH = -.2
+            elif CURRENT <= abs(GROWTH):
+                GROWTH = .2
+            CURRENT += GROWTH
+            delay = CURRENT
+
+        print 'Delaying call for %.1fs' % delay
+        time.sleep(delay)
         portforward.ProxyServer.dataReceived(self, data)
 
 
