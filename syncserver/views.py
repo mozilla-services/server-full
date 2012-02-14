d9365cdf3b98694f220d7efbe4ed5e60f7211c3c
Ryan Kelly <rkelly@mozilla.com>
2012-02-14 03:35:05 -0800

    Add ability to generate an BID assertion on the root page.
    This will go away soon, but it's handy for initial testing.

--- syncserver/views.py
+++ syncserver/views.py
@@ -26,6 +26,8 @@ delete_account = Service(name="delete_account", path="/weave-delete-account",
                          acl=lambda r: [(Allow, Authenticated, "authn")],
                          permission="authn")
 
+root = Service(name="root", path="/")
+
 @delete_account.get()
 def delete_account_form(request):
     """Returns a form for deleting the account"""
@@ -61,3 +63,44 @@ def do_delete_account(request):
         return text_response('Account removed.')
     else:
         return text_response('Deletion failed.')
+
+
+@root.get()
+def root_is_a_browserid_helper(request):
+    from pyramid.response import Response
+    return Response("""
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"
+        type="text/javascript"></script>
+<script src="https://browserid.org/include.js"
+        type="text/javascript"></script>
+</head>
+<body>
+<h1>Authentication Required</h1>
+<noscript>
+This page requires authentication via BrowserID.
+Unfortunately your browser does not support JavaScript which is required
+for BrowserID login.
+</noscript>
+<script type="text/javascript">
+$(function() {
+    // Generate login button in script, so it only appears if
+    // we're actually capable of doing it.
+    //
+    $("<img src='https://browserid.org/i/sign_in_blue.png' id='signin'" +
+      "     alt='sign-in button' />").appendTo($("body"));
+
+    // Fire up the BrowserID callback when clicked.
+    //
+    $("#signin").click(function() {
+        navigator.id.getVerifiedEmail(function(assertion) {
+            if (assertion) {
+                $("<div>").text(assertion).appendTo($("body"));
+            }
+        });
+    });
+});
+</script>
+</body>
+    """)
